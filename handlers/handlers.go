835e640b72a002cc38d0d26ae797bfc6648f0dd4
Richard Morrison <richard@rmorrison.net>
2019-03-03 21:39:20 +0000

    Move handlers to separate subfolder; use template to generate html

---
+++ handlers/handlers.go
@@ -0,0 +1,63 @@
+package handlers
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	subprocess "github.com/mozz100/screenserve/subprocess"
+)
+
+// HomeHandler handles GET requests for /
+func HomeHandler(sbpctx *subprocess.Context) func(http.ResponseWriter, *http.Request) {
+	indexTmpl := template.Must(template.ParseFiles("templates/index.html"))
+
+	homeHandler := func(w http.ResponseWriter, r *http.Request) {
+		indexTmpl.Execute(w, sbpctx)
+		log.Println("Responded with home page")
+	}
+	return homeHandler
+}
+
+// LaunchHandler handles POST requests
+func LaunchHandler(sbpctx *subprocess.Context) func(http.ResponseWriter, *http.Request) {
+	launchHandler := func(w http.ResponseWriter, r *http.Request) {
+		var desiredURL string
+		var slackAPI bool
+
+		r.ParseForm()
+		if url, ok := r.PostForm["url"]; ok {
+			desiredURL = url[0]
+		}
+		if url, ok := r.PostForm["text"]; ok {
+			desiredURL = url[0]
+			slackAPI = true
+		}
+		if clear, ok := r.PostForm["clear"]; ok && clear[0] == "1" {
+			desiredURL = ""
+		}
+
+		log.Printf("Launch page with desiredURL = '%v'. Slack: %v\n", desiredURL, slackAPI)
+		if sbpctx.Parameter != "" || desiredURL == "" {
+			sbpctx.Stop()
+		}
+		if desiredURL != "" {
+			sbpctx.StartWith(desiredURL)
+		}
+
+		if slackAPI {
+			log.Println("Responding with plain text 200")
+			if desiredURL == "" {
+				fmt.Fprint(w, "Cleared!")
+			} else {
+				fmt.Fprintf(w, "OK, showing %v :tv: :eyes:", desiredURL)
+			}
+
+		} else {
+			log.Println("Redirecting to home page")
+			http.Redirect(w, r, "/", http.StatusFound)
+		}
+	}
+	return launchHandler
+}
--- screenserve.go
+++ screenserve.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/mozz100/screenserve/handlers"
 	"github.com/mozz100/screenserve/subprocess"
 )
 
@@ -19,83 +19,9 @@ func main() {
 
 	sbpctx := subprocess.GetSubprocess(cmd)
 
-	launchHandler := func(w http.ResponseWriter, r *http.Request) {
-		var desiredURL string
-		var slackAPI bool
+	http.HandleFunc("/", handlers.HomeHandler(sbpctx))
+	http.HandleFunc("/launch/", handlers.LaunchHandler(sbpctx))
 
-		r.ParseForm()
-		if url, ok := r.PostForm["url"]; ok {
-			desiredURL = url[0]
-		}
-		if url, ok := r.PostForm["text"]; ok {
-			desiredURL = url[0]
-			slackAPI = true
-		}
-		if clear, ok := r.PostForm["clear"]; ok && clear[0] == "1" {
-			desiredURL = ""
-		}
-
-		log.Printf("Launch page with desiredURL = '%v'. Slack: %v\n", desiredURL, slackAPI)
-		if sbpctx.Parameter != "" || desiredURL == "" {
-			sbpctx.Stop()
-		}
-		if desiredURL != "" {
-			sbpctx.StartWith(desiredURL)
-		}
-
-		if slackAPI {
-			log.Println("Responding with plain text 200")
-			if desiredURL == "" {
-				fmt.Fprint(w, "Cleared!")
-			} else {
-				fmt.Fprintf(w, "OK, showing %v :tv: :eyes:", desiredURL)
-			}
-
-		} else {
-			log.Println("Redirecting to home page")
-			http.Redirect(w, r, "/", http.StatusFound)
-		}
-	}
-
-	homeHandler := func(w http.ResponseWriter, r *http.Request) {
-		cssClass := ""
-		if sbpctx.Parameter == "" {
-			cssClass = "d-none"
-		}
-		fmt.Fprintf(w, `
-		<!doctype html>
-		<html lang="en">
-		<head>
-			<!-- Required meta tags -->
-			<meta charset="utf-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-			
-			<!-- Bootstrap CSS -->
-			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-			
-			<title>Wall-e</title>
-		</head>
-		<body>
-			<div class="container mt-3">
-				<p class="%v">Currently showing: <code>%v</code></p>
-				<form method='POST' action='/launch/'>
-					<div class="form-group">
-						<input class="form-control" autofocus="autofocus" placeholder="Enter URL" name="url" type="url" value="%v" />
-						<small class="form-text text-muted">Include 'http' or 'https'</small>
-					</div>
-					
-					<button class="btn btn-primary" type='submit'>Fling</button>
-					&nbsp;or&nbsp;
-					<button class="btn btn-light" name='clear' value='1' type='submit'>Clear</button>
-				</form>
-			</div>
-		</body>
-		</html>`, cssClass, sbpctx.Parameter, sbpctx.Parameter)
-		log.Println("Responded with home page")
-	}
-
-	http.HandleFunc("/launch/", launchHandler)
-	http.HandleFunc("/", homeHandler)
 	log.Printf("Starting listening on port %v\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
